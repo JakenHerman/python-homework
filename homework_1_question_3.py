980c99ecffe1d609542ae9c7904437702192ff07
Jaken Herman <jch053@shsu.edu>
2015-08-27 23:03:29 -0500

    Update homework_1_question_3.py

--- homework_1_question_3.py
+++ homework_1_question_3.py
@@ -7,11 +7,11 @@
 #       LAB/QUIZ PROBLEM 3
 #
 
-user_name = input("What is your name?")
+user_name = input("What is your name? ")
 user_height_inches = eval(input("Please tell me your height in inches: "))
 user_height_feet = (user_height_inches / 12)
 
 print("Goodmorning " + user_name + ". You are " +
       str(user_height_feet)[0:1] + " feet and " +
-      str(user_height_feet)[2:] + " inches tall.")
+      str(user_height_feet)[2:3] + " inches tall.")
 
