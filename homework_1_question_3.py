116fa6700e5c9d5133d3694c661706b671e30beb
Jaken Herman <jch053@shsu.edu>
2015-08-27 21:29:27 -0500

    Create homework_1_question_3.py

---
+++ homework_1_question_3.py
@@ -0,0 +1,17 @@
+#__author__: Jaken Herman
+#__class___: COSC 2347
+#_professor: Dr. Ji
+#___date___: Aug 27, 2015
+
+#
+#       LAB/QUIZ PROBLEM 3
+#
+
+user_name = input("What is your name?")
+user_height_inches = eval(input("Please tell me your height in inches: "))
+user_height_feet = (user_height_inches / 12)
+
+print("Goodmorning " + user_name + ". You are " +
+      str(user_height_feet)[0:1] + " feet and " +
+      str(user_height_feet)[2:] + " inches tall.")
+
